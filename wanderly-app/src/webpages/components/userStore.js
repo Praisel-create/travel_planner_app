825b4fcf14e5017e6cdabe3353ee1ccbf8eb42a9
Praisel-create <praiseozoko3@gmail.com>
2025-04-01 08:40:20 +0100

    Delete wanderly-app directory

--- wanderly-app/src/webpages/components/userStore.js
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-const useUserStore = create((set) => ({
-    userName: "", //inital state of the username field before the user inputs their username
-    setUserName: (name) => set({userName: name})
-})
-);
-
-export default useUserStore;
