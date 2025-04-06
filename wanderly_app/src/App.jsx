171eb9d9bdd0de721a203796149f904208e984f6
Praisel <praiseozoko3@gmail.com>
2025-04-06 22:22:05 +0100

    Updating the Routing between pages

--- wanderly_app/src/App.jsx
+++ wanderly_app/src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import SearchPage from './pages/SearchPage';
+import DestinationsPage from './pages/DestinationsPage';
+import FlightOffersPage from './pages/FlightOffersPage';
+import ItineraryPage from './pages/ItineraryPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        {/* We'll add other routes tomorrow */}
+        <Route path='/search-page' element={<SearchPage />}/>
+        <Route path='/destinations-page' element={<DestinationsPage />}/>
+        <Route path='/flight-search-page' element={<FlightOffersPage/>}/>
+        <Route path='/itinerary-page' element={<ItineraryPage/>}/>
       </Routes>
     </BrowserRouter>
   );
